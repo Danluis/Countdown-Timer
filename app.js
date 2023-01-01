88b5b2b4b5d1b1addd0f6ebdce02e5c2dad84411
Danluis <Danluis2a25@gmail.com>
2022-12-31 22:28:39 -0400

    Project in BETA

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-const newYears = '1st jan 2023';
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newYears = "1 jan 2023";
 
 function countdown(){
-     const newYearDate = new Date(newYears);
+     const newYearsDate = new Date(newYears);
      const currentDate = new Date();
 
+     const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+     const days = Math.floor(totalSeconds / 3600 /24);
+
+     const hours = Math.floor(totalSeconds / 3600 % 24);
+     
+     const mins = Math.floor(totalSeconds / 60) % 60;
+
+     const seconds =  Math.floor(totalSeconds) % 60;
+
+     daysEl.innerHTML = days;
+     hoursEl.innerHTML = hours;
+     minsEl.innerHTML = mins;
+     secondsEl.innerHTML = seconds;
 
 }
+
+countdown();
+
+setInterval(countdown, 1000)
